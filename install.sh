da5000452fe3b69656c6ec9ffbfef1346b293274
avsigaev <avsigaev@gmail.com>
2019-04-25 13:12:42 +0300

    init

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 # Exit script as soon as a command fails.
 set -o errexit
 
+if [ -n $(echo $(pgrep Xorg)) ]; then
+    echo 'ERROR: Xorg server is not running, exit'
+    exit 1
+fi
+
 github_url='https://raw.githubusercontent.com/avsigaev/fan-control'
 branch='master'
 
