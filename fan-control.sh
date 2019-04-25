b5b9468764c8855f9d62b89f299720b33c343f29
avsigaev <avsigaev@gmail.com>
2019-04-25 14:02:07 +0300

    init

--- fan-control.sh
+++ fan-control.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if [ -z $(pgrep Xorg) ]; then
-    echo 'ERROR: Xorg server is not running, exit'
-    exit 1
-fi
+while [ -z $(pgrep Xorg) ]; do
+    echo 'ERROR: Xorg server is not running, waiting...'
+    sleep 5
+done
 
 export $(cat /etc/sonm/fan-control.txt)
 
